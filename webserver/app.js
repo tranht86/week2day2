3c8fe095adf6be796debd13a868ac1020571a62c
tranht86 <tran_ht@hotmail.com>
2017-04-11 16:21:46 +0000

    Handle more than one web request

--- webserver/app.js
+++ webserver/app.js
@@ -3,7 +3,14 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 function requestHandler(request, response) {
-  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+  if (request.url == "/") {
+    response.end("Welcome!");
+  } else if (request.url == "/urls") {
+    response.end("www.lighthouselabs.ca\nwww.google.com");
+  } else {
+    response.statusCode = 404;
+    response.end("Unknown Path")
+  }
 }
 
 var server = http.createServer(requestHandler);
